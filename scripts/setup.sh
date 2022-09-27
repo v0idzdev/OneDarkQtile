0d3f0a8d749c8109b41254dcd8f9aefac3277a73
Matthew Flegg <88111643+matthewflegg@users.noreply.github.com>
2022-09-27 10:26:17 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,17 +6,14 @@ cp -a config/alacritty/ ~/.config/alacritty
 
 # Setup GTK themes
 pacman -S git
-cd ~
 git clone https://github.com/UnnatShaneshwar/AtomOneDarkTheme
 cp -r AtomOneDarkTheme/ /usr/share/themes/
 rm -rf AtomOneDarkTheme  # Clean up
 
 # Install rofi power menu
-cd ~
 git clone https://github.com/jluttine/rofi-power-menu
 cp rofi-power-menu/ /usr/bin/
 rm -rf rofi-power-menu
-cd -
 cp themes/one-dark.rasi /usr/share/rofi/themes/
 
 # Setup wallpapers
