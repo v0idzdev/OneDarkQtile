1fc1e5b0f25f77f7b579944f02c67c3ed94cff5b
Matthew Flegg <88111643+matthewflegg@users.noreply.github.com>
2022-09-27 10:39:05 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,9 +12,6 @@ sudo cp -r AtomOneDarkTheme/ /usr/share/themes/
 rm -rf AtomOneDarkTheme  # Clean up
 
 # Install rofi power menu
-#git clone https://github.com/jluttine/rofi-power-menu
-#sudo cp -r rofi-power-menu/ /usr/bin/
-#rm -rf rofi-power-menu
 sudo cp themes/one-dark.rasi /usr/share/rofi/themes/
 
 # Setup wallpapers
